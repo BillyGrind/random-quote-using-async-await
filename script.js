d22e0836a401e408a92fc8bcde1b260134d85057
BillyGrind <denayeranthony623@gmail.com>
2023-05-19 19:07:05 +0200

    try/catch ok , need loading and age

--- script.js
+++ script.js
@@ -10,25 +10,47 @@ let newDiv = document.createElement("div");
 newDiv.classList.add("quote-container");
 
 //request API
-fetch("https://thatsthespir.it/api")
-  .then((response) => response.json())
-  .then((json) => {
-    //Author
-    let author = json.author;
-    //blockQuote
-    let quoteblock = json.quote;
-    //Image
-    let picture = json.photo;
-    let content = `
-    <h1 class="quote-title">${author}</h1>
-    <image src="${picture}" class="quote-image"></image>
-    <blockquote class="quote">${quoteblock}</blockquote>
-    <button class="quote-button">Refresh</button>
-    `;
-    newDiv.innerHTML += content;
-    body.appendChild(newDiv);
-    let buttonRefresh = document.getElementsByClassName("quote-button")[0];
-    buttonRefresh.addEventListener("click", () => {
-      location.reload();
+
+async function fetchQuote(){
+  try{
+    fetch("https://thatsthespir.it/api")
+    .then((response) => response.json())
+    .then((json) => {
+      //Author
+      let author = json.author;
+      //age
+      let age = json.age;
+      // if (age === null){
+      //   fetch("https://thatsthespir.it/api")
+      //   .then((response) => response.json())
+      //   .then((json) => {
+      //     let age = age.json;
+      //   });
+      // } 
+      //blockQuote
+      let quoteblock = json.quote;
+      //Image
+      let picture = json.photo;
+      let content = `
+      <h1 class="quote-title">${author}</h1>
+      <h2 class="quote-age">${age}</h2>
+      <image src="${picture}" class="quote-image"></image>
+      <blockquote class="quote">${quoteblock}</blockquote>
+      <button class="quote-button">Refresh</button>
+      `;
+      newDiv.innerHTML += content;
+      body.appendChild(newDiv);
+      let buttonRefresh = document.getElementsByClassName("quote-button")[0];
+      buttonRefresh.addEventListener("click", () => {
+        location.reload();
+      });
     });
-  });
+  }catch(error){
+    let content = `
+    <p>Error</p>
+    `
+    newDiv.innerHTML += content;
+  }
+}
+
+fetchQuote();
